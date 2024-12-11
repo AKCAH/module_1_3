e5efe156070a2b448087148494c943e34349bfb5
Axan <a.davletkujin@gmail.com>
2024-12-11 20:58:14 +0500

    Динамическая типизация

---
+++ module_1_3.py
@@ -0,0 +1,19 @@
+# по заданию так:
+name = 'Аксан'
+print(name)
+age = 38
+print(age)
+age = age + 1
+print(age)
+is_student = True
+print(is_student)
+
+# но в примере так:
+name = 'Аксан'
+print('name', name)
+age = 38
+print('age', age)
+age = age + 1
+print('new_age', age)
+is_student = True
+print('is_Student', is_student)
